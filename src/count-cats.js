e7498d4c39609495ef27efb74f217d836740ab3d
dzzuze <notinsidik@gmail.com>
2025-11-07 22:35:10 +0300

    feat: implement countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let catCount = 0;
+  for (let i = 0; i < matrix.length; i += 1){
+    for (let j = 0; j < matrix[i].length; j += 1){
+      if (matrix[i][j] === "^^")
+        catCount += 1;
+    }
+  }
+  return catCount;
 }
 
 module.exports = {
