c074913230c0124829dfe5b76850433ab44cdf30
dzzuze <notinsidik@gmail.com>
2025-11-09 01:39:38 +0300

    feat: implement carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string'){
+    return false;
+  }
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) ||
+    activity <= 0 ||
+    activity > MODERN_ACTIVITY ||
+    !isFinite(activity)) {
+    return false;
+  }
+
+  const valueT = (Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD) / Math.log(2);
+  return Math.ceil(valueT);
 }
 
 module.exports = {
