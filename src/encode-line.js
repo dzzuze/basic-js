cfc41d436e23099d4c0db329c4e50a92242b1327
dzzuze <notinsidik@gmail.com>
2025-11-10 21:57:30 +0300

    feat: implement encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  if (str === '') return '';
+  let result = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      if (count > 1) {
+        result += count + str[i]; 
+      } else {
+        result += str[i]; 
+      }
+      count = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = {
