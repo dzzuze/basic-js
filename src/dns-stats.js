f68e2f71088e948e918e1abfc889a059cfc61b18
dzzuze <notinsidik@gmail.com>
2025-11-10 21:52:04 +0300

    feat: implement dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  const stats = {};
+  for (let domain of domains) {
+    const parts = domain.split('.').reverse();
+    let currentSubdomain = '';
+    for (let part of parts) {
+      currentSubdomain += '.' + part;
+      stats[currentSubdomain] = (stats[currentSubdomain] || 0) + 1;
+    }
+  }
+  return stats;
 }
 
 module.exports = {
